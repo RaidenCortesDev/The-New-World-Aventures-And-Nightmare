3a1f4bab8908edf34d93d060be164cbbc916943a
Brandon Cortes Islas <dracko.cortes.99@gmail.com>
2026-03-10 20:40:28 -0600

    test: Planificación de carpeta

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,15 +17,11 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    /*Component.ConditionalRender({
+    Component.ConditionalRender({
       component: Component.Breadcrumbs(),
       condition: (page) => page.fileData.slug !== "index",
-    }),*/
-    Component.ConditionalRender({
-      component: Component.ArticleTitle(),
-      condition: (page) => page.fileData.slug !== "index",
     }),
-    //Component.ArticleTitle(),
+    Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
@@ -42,30 +38,10 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    //Component.Explorer(),
-    Component.Explorer({ // Explorador limpio (sin archivos repetidos)
-      filterFn: (node) => {
-        const isIndex = node.name.toLowerCase() === "index"
-        const isDuplicate = node.name === node.parent?.name
-        return !isIndex && !isDuplicate
-      },
-      useSavedState: true,
-    }),
+    Component.Explorer(),
   ],
   right: [
-    //Component.Graph(),
-    Component.Graph({ // La constelación de vuelta a la derecha y con profundidad
-      localGraph: {
-        depth: 2, 
-        linkDistance: 50,
-        repulsion: 400,
-      },
-      globalGraph: {
-        depth: -1,
-        linkDistance: 100,
-        repulsion: 800,
-      },
-    }),
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
@@ -73,10 +49,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [/*Component.Breadcrumbs()*/Component.Breadcrumbs({
-    hideOnRoot: true,
-    showCurrentPage: false, // Esto evita que el último nombre se repita
-  }), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
